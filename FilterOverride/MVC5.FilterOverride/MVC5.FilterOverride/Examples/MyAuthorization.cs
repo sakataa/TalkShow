7a12024df0071c9138b30596b7e6f18a8a13e94f
Walter Tran <walter.tran@nexcel.vn>
2015-09-30 08:01:50 +0700

    ASP.NET 5 - TalkShow -  Add Filter Override demo

---
+++ FilterOverride/MVC5.FilterOverride/MVC5.FilterOverride/Examples/MyAuthorization.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+
+namespace MVC5.FilterOverride.Examples
+{
+    public class MyAuthorization: FilterAttribute, IOverrideFilter
+    {
+
+        public Type FiltersToOverride
+        {
+            get 
+            { 
+                return typeof(IAuthorizationFilter); 
+            }
+        }
+    }
+}
---
+++ FilterOverride/MVC5.FilterOverride/MVC5.FilterOverride/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5.FilterOverride.MvcApplication" Language="C#" %>
