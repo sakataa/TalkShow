a24a7888432b2faa642239e1ca9062728012b9b6
sakataa <tientm.ctt@gmail.com>
2015-10-06 00:17:20 +0700

    ASP.NET 5 - TalkShow -  Route mixing to MVC6

---
+++ AttributeRouting/ASP5.AttributeRouting/src/MVC6/Controllers/RouteMixingController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVC6.Controllers
+{
+    public class RouteMixingController : Controller
+    {
+        [HttpGet("asp5/Mixing/Default/{id:int=99}")]
+        public IActionResult Index(int id)
+        {
+            ViewBag.Message = "[HttpGet('asp5 / Mixing / Default')]" + id;
+            return View();
+        }
+
+        [HttpPost("asp5/Mixing/Default")]
+        public IActionResult PostIndex(int id)
+        {
+            return Content("[HttpPost('asp5 / Mixing / Default')]" + id);
+        }
+    }
+}
