9ff697d0a02051ec868f9bb3472fa4aee5491900
sakataa <tientm.ctt@gmail.com>
2015-10-14 23:51:34 +0700

    ASP.NET 5 - TalkShow -  Add ASP.NET 5 - Filter overriding

---
+++ FilterOverride/ASP5.FilterOverriding/src/ASP5.FilterOverriding/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+
+namespace ASP5.FilterOverriding.Controllers
+{
+    [Authorize(Roles= "Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [Authorize(Roles = "User")]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
---
+++ FilterOverride/ASP5.FilterOverriding/src/ASP5.FilterOverriding/Services/OverrideFriendlyFilterProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace ASP5.FilterOverriding.Services
+{
+    public class OverrideFriendlyFilterProvider : INestedProvider<FilterProviderContext>
+    {
+        //all framework providers have negative orders, so ours will come later
+        public int Order => 0;
+
+        public void Invoke(FilterProviderContext context, Action callNext)
+        {
+            //let the whole provider pipeline run first
+            if (callNext != null)
+            {
+                callNext();
+            }
+
+            if (context.ActionContext.ActionDescriptor.FilterDescriptors != null)
+            {
+                var overrideFilters = context.Results.Where(filterItem => filterItem.Filter is OverrideFilter).ToArray();
+                foreach (var overrideFilter in overrideFilters)
+                {
+                    context.Results.RemoveAll(filterItem =>
+                    filterItem.Descriptor.Filter.GetType() == ((OverrideFilter)overrideFilter.Filter).Type &&
+                    filterItem.Descriptor.Scope <= overrideFilter.Descriptor.Scope);
+                }
+            }
+        }
+    }
+
+    public interface INestedProvider<T>
+    {
+        int Order { get; }
+        void Invoke(T context, Action callNext);
+    }
+    public class OverrideFilter : ActionFilterAttribute
+    {
+        public Type Type { get; set; }
+    }
+}
