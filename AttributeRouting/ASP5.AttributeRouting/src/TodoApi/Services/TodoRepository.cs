52a819100434ba2357b7150c0ca7af89167d888f
sakataa <tientm.ctt@gmail.com>
2015-10-14 18:11:38 +0700

    ASP.NET 5 - TalkShow -  Add Todo example

---
+++ AttributeRouting/ASP5.AttributeRouting/src/TodoApi/Services/TodoRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using TodoApi.Models;
+
+namespace TodoApi.Services
+{
+    public class TodoRepository : ITodoRepository
+    {
+        static readonly ConcurrentDictionary<string, TodoItem> Todos = new ConcurrentDictionary<string, TodoItem>();
+
+        public TodoRepository()
+        {
+            Add(new TodoItem { Name = "Item1" });
+        }
+
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return Todos.Values;
+        }
+
+        public void Add(TodoItem item)
+        {
+            item.Key = Guid.NewGuid().ToString();
+            Todos[item.Key] = item;
+        }
+
+        public TodoItem Find(string key)
+        {
+            TodoItem item;
+            Todos.TryGetValue(key, out item);
+            return item;
+        }
+
+        public TodoItem Remove(string key)
+        {
+            TodoItem item;
+            Todos.TryGetValue(key, out item);
+            Todos.TryRemove(key, out item);
+            return item;
+        }
+
+        public void Update(TodoItem item)
+        {
+            Todos[item.Key] = item;
+        }
+    }
+}
