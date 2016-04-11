76aa565da52dea5a0b10cc4de11dc19f7da04081
Walter Tran <walter.tran@nexcel.vn>
2016-04-11 17:36:56 +0700

    ES6 - Remove circular module dependency

--- ES6/Organization/module/webpack.config.js
+++ ES6/Organization/module/webpack.config.js
@@ -5,8 +5,8 @@
      entry: {
          basic: './import/importing.js',
          defaultImporting: './import/default-importing.js',
-         exporting_anotherfile: './import/importing-anotherfile.js',
-         circular_module_dependency: './import/circular-module-dependency.js'
+         exporting_anotherfile: './import/importing-anotherfile.js'
+         //circular_module_dependency: './import/circular-module-dependency.js'
      },
      output: {
          path: path.resolve(__dirname, './'),
