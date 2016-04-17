dacb95d5a9ed7090cace76d232e35c8064939970
sakataa <tientm.ctt@gmail.com>
2016-04-18 00:27:03 +0700

    ES6 - Make demo about asynchronous with generator

--- ES6/AsyncFlowControl/Ajax/util.js
+++ ES6/AsyncFlowControl/Ajax/util.js
@@ -1,15 +1,15 @@
-let Util = (function() {
+let Util = (function () {
     let rootUrl = "Data";
     let chapterRootUrl = rootUrl + "/Chapters";
-    const fakeSlowNetwork = 1;
+    const fakeSlowNetwork = 0;
     const waitingTime = 4000;
-    
+
     function wait(ms) {
         return new Promise(function (resolve) {
             setTimeout(resolve("wait"), ms * fakeSlowNetwork);
         });
     }
-    
+
     function getData(url) {
         let fakeNetworkWait = wait(waitingTime);
         let requestPromise = new Promise(function (resolve, reject) {
@@ -33,13 +33,13 @@ let Util = (function() {
 
             request.send();
         });
-        
-        return Promise.all([fakeNetworkWait, requestPromise]).then(function(results) {
+
+        return Promise.all([fakeNetworkWait, requestPromise]).then(function (results) {
             return results[1];
-        }); 
+        });
     };
-    
-    function getChapter(index){   
+
+    function getChapter(index) {
         getJSON(url).then(function (story) {
             let fullChapterUrl = getChapterUrl(story.chapterUrls[index - 1]);
             return getJSON(fullChapterUrl);
@@ -47,11 +47,11 @@ let Util = (function() {
             console.log(`Got chapter ${index}!`, chapter);
         });
     };
-    
+
     let storyPromise;
-    function getChapterWithReusingPromise(index){
+    function getChapterWithReusingPromise(index) {
         storyPromise = storyPromise || getJSON(url); //reuse promise, story.json only fetch once
-        
+
         storyPromise.then(function (story) {
             let fullChapterUrl = getChapterUrl(story.chapterUrls[index - 1]);
             return getJSON(fullChapterUrl);
@@ -59,7 +59,7 @@ let Util = (function() {
             console.log(`Got chapter ${index}!`, chapter);
         });
     };
-    
+
     function getChaptersAsync() {
         storyPromise = getJSON(url);
         let chapterPromises = [];
@@ -74,23 +74,23 @@ let Util = (function() {
             return Promise.all(chapterPromises);
         })
     };
-    
+
     function getChapterUrl(chapterName) {
         return `${chapterRootUrl}/${chapterName}`;
     }
-    
+
     function getUrl(fileName) {
         return rootUrl + `/${fileName}`;
     };
-    
+
     function getJSON(url) {
         return getData(url).then(JSON.parse);
     };
-    
+
     function getChaptersSync() {
         let storyUrl = getUrl("story.json");
         let story = getJsonSync(storyUrl);
-        
+
         story.chapterUrls.forEach(function (chapterUrl) {
             let fullChapterUrl = getChapterUrl(chapterUrl);
             let chapter = getJsonSync(fullChapterUrl);
@@ -98,11 +98,11 @@ let Util = (function() {
         });
 
     };
-    
+
     function getSync(url) {
         let startTime = Date.now();
         let waiting = waitingTime * fakeSlowNetwork;
-        
+
         let req = new XMLHttpRequest();
         req.open('get', url, false);
         req.send();
@@ -120,7 +120,7 @@ let Util = (function() {
     function getJsonSync(url) {
         return JSON.parse(getSync(url));
     }
-    
+
     return {
         rootUrl: rootUrl,
         chapterRootUrl: chapterRootUrl,
@@ -130,7 +130,6 @@ let Util = (function() {
         getChaptersAsync() { return getChaptersAsync() },
         getChaptersSync() { getChaptersSync(); },
         getUrl(fileName) { return getUrl(fileName) },
-        getJSON(url) { return getJSON(url) }      
+        getJSON(url) { return getJSON(url) }
     }
 })();
-
