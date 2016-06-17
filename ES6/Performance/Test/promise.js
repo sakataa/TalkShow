e40d393cd8b95fcb3046db60522bdb1d61f06a4d
sakataa <tientm.ctt@gmail.com>
2016-06-18 05:47:01 +0700

    ES6 - Test performance for Template Literal and Promise

---
+++ ES6/Performance/Test/promise.js
@@ -0,0 +1,38 @@
+let url = "https://cdn.rawgit.com/sakataa/TalkShow/ES6/ES6/Demo/Organization/Generator/Asynchronous/Data/story.json";
+let promiseCount = 1000;
+
+function testES5Promise(){
+    testES5(function(){
+        for(let i = 0; i < promiseCount; i++){
+            $.ajax({
+                url: url
+            })
+            .then(function( data, textStatus, jqXHR ) {
+                //console.log("ES5: " + i);              
+            });
+        }
+    })
+}
+
+function testES6Promise(){
+    testES6(function(){
+        for(let i = 0; i < promiseCount; i++){
+            new Promise(function (resolve, reject) {
+                let request = new XMLHttpRequest();
+                request.open("GET", url);
+
+                request.onload = function () {
+                    if (request.status === 200) {
+                        resolve(request.response);
+                    }
+                    else {
+                        reject(Error(request.statusText));
+                    }
+                };
+
+                request.send();
+            })
+            .then( (data) => { /*console.log("ES6: " + i);*/ } );
+        }
+    })
+}
---
+++ ES6/Performance/Test/template-literal.js
@@ -0,0 +1,19 @@
+let data = ["Muller", "Bruce", "Alex", "Bravo", "Joseph", "Forrest", "Walter"];
+let templateLiteralCount = 100000000;
+function testES5TemplateLiteral(){
+    testES5(function(){
+        for(var i = 0; i < templateLiteralCount; i++){
+            data[0] + ' ' + data[1] + ' ' + data[2] + ' ' + data[3]+ '\n' 
+            + ' ' + data[4]+ ' ' + data[5]+ ' ' + data[6];
+        }
+    })
+}
+
+function testES6TemplateLiteral(){
+    testES6(function(){
+        for(var i = 0; i < templateLiteralCount; i++){
+            `${data[0]} ${data[1]} ${data[2]} ${data[3]}
+             ${data[4]} ${data[5]} ${data[6]}`;
+        }
+    })
+}
